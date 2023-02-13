86959ea2fad2b9df9cace4dd2cb79e5f0b21def6
Christoph Müllner <christoph.muellner@vrull.eu>
2023-02-13 14:39:49 +0100

    RISC-V: Add Zvks ISA extension support
    This commit adds support for the Zvks ISA extension,
    which is part of the vector crypto extensions.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ gas/testsuite/gas/riscv/zvks.d
@@ -0,0 +1,10 @@
+#as: -march=rv64gc_zvks
+#objdump: -dr
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+0+000 <.text>:
+[ 	]+[0-9a-f]+:[ 	]+86802277[ 	]+vsm4k.vi[ 	]+v4,v8,0
+[ 	]+[0-9a-f]+:[ 	]+ae802277[ 	]+vsm3c.vi[ 	]+v4,v8,0
---
+++ gas/testsuite/gas/riscv/zvks.s
@@ -0,0 +1,2 @@
+	vsm4k.vi v4, v8, 0
+	vsm3c.vi v4, v8, 0
