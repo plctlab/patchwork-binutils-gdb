8a05409c13cdec5cd2682e36adfd00f9d3c54549
Christoph Müllner <christoph.muellner@vrull.eu>
2023-02-13 14:39:46 +0100

    RISC-V: Add Zvkn ISA extension support
    This commit adds support for the Zvkn ISA extension,
    which is part of the vector crypto extensions.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ gas/testsuite/gas/riscv/zvkn.d
@@ -0,0 +1,10 @@
+#as: -march=rv64gc_zvkn
+#objdump: -dr
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+0+000 <.text>:
+[ 	]+[0-9a-f]+:[ 	]+a280a277[ 	]+vaesdf.vv[ 	]+v4,v8
+[ 	]+[0-9a-f]+:[ 	]+ba862277[ 	]+vsha2ch.vv[ 	]+v4,v8,v12
