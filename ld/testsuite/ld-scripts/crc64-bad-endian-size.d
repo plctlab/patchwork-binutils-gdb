9942f5fc238baad0d160d8e32c97290d6e9c502f
Ulf Samuelsson <ulf@emagii.com>
2023-03-03 15:46:59 +0100

    DIGEST: testsuite
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

---
+++ ld/testsuite/ld-scripts/crc64-bad-endian-size.d
@@ -0,0 +1,3 @@
+#source: crc64-bad-endian-size.s
+#ld: -T crc64-bad-endian-size.t
+# error: .*: bad 'endian' .* in digest label.*
---
+++ ld/testsuite/ld-scripts/crc64-bad-label-2.d
@@ -0,0 +1,3 @@
+#source: crc64-bad-label-2.s
+#ld: -T crc64-bad-label-2.t
+# error: .*: Illegal label .* in digest command.*
