bminor/binutils-gdb

commit e87fb6a6d0cdfc0e9c471b7825c20c238c2cf506

x86/gas: support quoted address scale factor in AT&T syntax
An earlier attempt (e68c3d59acd0 ["x86: better respect quotes in
parse_operands()"]) needed undoing (cc0f96357e0b ["x86: permit
parenthesized expressions again as addressing scale factor"]) as far its
effect here went. As indicated back then, the issue is the backwards
scanning of the operand string to find the matching opening parenthesis.
Switch to forward scanning, finding the last outermost unquoted opening
parenthesis (which is the one matching the trailing closing one).

gas/config/tc-i386.c
@@ -11589,25 +11589,32 @@ i386_att_operand (char *operand_string)
       if (*base_string == ')')
 	{
 	  char *temp_string;
-	  unsigned int parens_not_balanced = 1;
+	  unsigned int parens_not_balanced = 0;
+	  bool in_quotes = false;
 
 	  /* We've already checked that the number of left & right ()'s are
-	     equal, so this loop will not be infinite.  */
-	  do
+	     equal, and that there's a matching set of double quotes.  */
+	  end_op = base_string;
+	  for (temp_string = op_string; temp_string < end_op; temp_string++)
 	    {
-	      base_string--;
-	      if (*base_string == ')')
-		parens_not_balanced++;
-	      if (*base_string == '(')
-		parens_not_balanced--;
+	      if (*temp_string == '\\' && temp_string[1] == '"')
+		++temp_string;
+	      else if (*temp_string == '"')
+		in_quotes = !in_quotes;
+	      else if (!in_quotes)
+		{
+		  if (*temp_string == '(' && !parens_not_balanced++)
+		    base_string = temp_string;
+		  if (*temp_string == ')')
+		    --parens_not_balanced;
+		}
 	    }
-	  while (parens_not_balanced && *base_string != '"');
 
 	  temp_string = base_string;
 
 	  /* Skip past '(' and whitespace.  */
-	  if (*base_string == '(')
-	    ++base_string;
+	  gas_assert (*base_string == '(');
+	  ++base_string;
 	  if (is_space_char (*base_string))
 	    ++base_string;
 

gas/testsuite/gas/i386/sib-intel.d
@@ -34,6 +34,10 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	8b 04 40             	mov    eax,DWORD PTR \[eax\+eax\*2\]
 [ 	]*[a-f0-9]+:	8b 04 80             	mov    eax,DWORD PTR \[eax\+eax\*4\]
 [ 	]*[a-f0-9]+:	8b 04 c0             	mov    eax,DWORD PTR \[eax\+eax\*8\]
+[ 	]*[a-f0-9]+:	8b 14 08             	mov    edx,DWORD PTR \[eax\+ecx\*1\]
+[ 	]*[a-f0-9]+:	8b 14 48             	mov    edx,DWORD PTR \[eax\+ecx\*2\]
+[ 	]*[a-f0-9]+:	8b 14 88             	mov    edx,DWORD PTR \[eax\+ecx\*4\]
+[ 	]*[a-f0-9]+:	8b 14 c8             	mov    edx,DWORD PTR \[eax\+ecx\*8\]
 [ 	]*[a-f0-9]+:	8b 04 25 e2 ff ff ff 	mov    eax,DWORD PTR \[eiz\*1-0x1e\]
 [ 	]*[a-f0-9]+:	8b 04 65 e2 ff ff ff 	mov    eax,DWORD PTR \[eiz\*2-0x1e\]
 [ 	]*[a-f0-9]+:	8b 04 a5 e2 ff ff ff 	mov    eax,DWORD PTR \[eiz\*4-0x1e\]

gas/testsuite/gas/i386/sib.d
@@ -33,6 +33,10 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	8b 04 40             	mov    \(%eax,%eax,2\),%eax
 [ 	]*[a-f0-9]+:	8b 04 80             	mov    \(%eax,%eax,4\),%eax
 [ 	]*[a-f0-9]+:	8b 04 c0             	mov    \(%eax,%eax,8\),%eax
+[ 	]*[a-f0-9]+:	8b 14 08             	mov    \(%eax,%ecx,1\),%edx
+[ 	]*[a-f0-9]+:	8b 14 48             	mov    \(%eax,%ecx,2\),%edx
+[ 	]*[a-f0-9]+:	8b 14 88             	mov    \(%eax,%ecx,4\),%edx
+[ 	]*[a-f0-9]+:	8b 14 c8             	mov    \(%eax,%ecx,8\),%edx
 [ 	]*[a-f0-9]+:	8b 04 25 e2 ff ff ff 	mov    -0x1e\(,%eiz,1\),%eax
 [ 	]*[a-f0-9]+:	8b 04 65 e2 ff ff ff 	mov    -0x1e\(,%eiz,2\),%eax
 [ 	]*[a-f0-9]+:	8b 04 a5 e2 ff ff ff 	mov    -0x1e\(,%eiz,4\),%eax

gas/testsuite/gas/i386/sib.s
@@ -30,6 +30,14 @@ foo:
 	mov	(%eax, %eax, (1 << 1)), %eax
 	mov	(%eax, %eax, (1 << 2)), %eax
 	mov	(%eax, %eax, (1 << 3)), %eax
+	.equ "scale(1)", 1
+	mov	(%eax, %ecx, "scale(1)"), %edx
+	.equiv "scale[2]", 2
+	mov	(%eax, %ecx, "scale[2]"), %edx
+	.eqv "scale{4}", 4
+	mov	(%eax, %ecx, "scale{4}"), %edx
+	.set "scale<8>", 8
+	mov	(%eax, %ecx, "scale<8>"), %edx
 	.intel_syntax noprefix
         mov    eax,DWORD PTR [eiz*1-30]
         mov    eax,DWORD PTR [eiz*2-30]

plctlab/patchwork-binutils-gdb

commit 671e779dd9395772ee61dfb98d3ed34c84ef1f4c

x86: Check unbalanced braces in memory reference
Check unbalanced braces in memory reference to avoid assembler crash
caused by

commit e87fb6a6d0cdfc0e9c471b7825c20c238c2cf506
Author: Jan Beulich <jbeulich@suse.com>
Date:   Wed Oct 5 09:16:24 2022 +0200

    x86/gas: support quoted address scale factor in AT&T syntax

	PR gas/30248
	* config/tc-i386.c (i386_att_operand): Check unbalanced braces
	in memory reference.
	* testsuite/gas/i386/i386.exp: Run pr30248.
	* testsuite/gas/i386/pr30248.d: New file.
	* testsuite/gas/i386/pr30248.err: Likewise.
	* testsuite/gas/i386/pr30248.s: Likewise.

gas/config/tc-i386.c
@@ -11613,7 +11613,11 @@ i386_att_operand (char *operand_string)
 	  temp_string = base_string;
 
 	  /* Skip past '(' and whitespace.  */
-	  gas_assert (*base_string == '(');
+	  if (*base_string != '(')
+	    {
+	      as_bad (_("unbalanced braces"));
+	      return 0;
+	    }
 	  ++base_string;
 	  if (is_space_char (*base_string))
 	    ++base_string;

gas/testsuite/gas/i386/i386.exp
@@ -752,6 +752,7 @@ run_dump_test pr19498
 run_list_test "nop-bad-1" ""
 run_list_test "unspec" ""
 run_dump_test "fp"
+run_dump_test pr30248
 if {[is_elf_format] || [istarget "*-*-vxworks*"]} then {
     run_list_test_stdin "list-1" "-al"
     run_list_test_stdin "list-2" "-al"

gas/testsuite/gas/i386/pr30248.d
@@ -1,0 +1,2 @@
+#as:
+#error_output: pr30248.err

gas/testsuite/gas/i386/pr30248.err
@@ -1,0 +1,5 @@
+#failif
+
+#...
+.*Internal error.*
+#pass

gas/testsuite/gas/i386/pr30248.s
@@ -1,0 +1,2 @@
+	.text
+	lgs ")"""