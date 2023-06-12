903d26264e0ed4d1890d3bfac664a871d1bafbcb
Feiyang Chen <chenfeiyang@loongson.cn>
2023-06-12 16:36:49 +0800

    LoongArch: Add fcsr register names support
    Add fcsr register names support for fcsr move instructions.
    
    gas/ChangeLog:
    
            * config/tc-loongarch.c:
            (loongarch_fc_normal_name): New definition.
            (loongarch_single_float_opcodes): Modify `movgr2fcsr` and
            `movfcsr2gr`.
    
    include/ChangeLog:
    
            * opcode/loongarch.h (loongarch_fc_normal_name): New extern.
    
    opcodes/ChangeLog:
    
            * opcodes/loongarch-dis.c (loongarch_after_parse_args): Add
            fcsr register names support.
            * opcodes/loongarch-opc.c (loongarch_args_parser_can_match_arg_helper):
            Likewise.
    
    Signed-off-by: Feiyang Chen <chenfeiyang@loongson.cn>

--- gas/config/tc-loongarch.c
+++ gas/config/tc-loongarch.c
@@ -207,6 +207,7 @@ md_parse_option (int c, const char *arg)
 
 static struct htab *r_htab = NULL;
 static struct htab *f_htab = NULL;
+static struct htab *fc_htab = NULL;
 static struct htab *c_htab = NULL;
 static struct htab *cr_htab = NULL;
 static struct htab *v_htab = NULL;
@@ -268,6 +269,18 @@ loongarch_after_parse_args ()
 
       for (i = 0; i < ARRAY_SIZE (loongarch_f_normal_name); i++)
 	str_hash_insert (f_htab, loongarch_f_normal_name[i], (void *) (i + 1),
+			 0);
+
+      if (!fc_htab)
+	fc_htab = str_htab_create (), str_hash_insert (fc_htab, "", 0, 0);
+
+      for (i = 0; i < ARRAY_SIZE (loongarch_fc_normal_name); i++)
+	str_hash_insert (fc_htab, loongarch_fc_normal_name[i], (void *) (i + 1),
+			 0);
+
+      /* Add general purpose registers for backward compatibility.  */
+      for (i = 0; i < ARRAY_SIZE (loongarch_r_normal_name); i++)
+	str_hash_insert (fc_htab, loongarch_r_normal_name[i], (void *) (i + 1),
 			 0);
 
       if (!c_htab)
@@ -638,7 +651,14 @@ loongarch_args_parser_can_match_arg_helper (char esc_ch1, char esc_ch2,
       ret = imm - 1;
       break;
     case 'f':
-      imm = (intptr_t) str_hash_find (f_htab, arg);
+      switch (esc_ch2)
+	{
+	case 'c':
+	  imm = (intptr_t) str_hash_find (fc_htab, arg);
+	  break;
+	default:
+	  imm = (intptr_t) str_hash_find (f_htab, arg);
+	}
       ip->match_now = 0 < imm;
       ret = imm - 1;
       break;
