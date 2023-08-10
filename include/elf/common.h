322808945445a2880a56185e61cb65a9d77df5fe
Greg Savin <greg.savin@sifive.com>
2023-08-10 15:11:22 -0700

    Propagate NT_RISCV_VECTOR from Linux kernel headers to binutils. The value is identical to pre-existing NT_RISCV_CSR but the note names different (NT_RISCV_CSR is "GDB" and NT_RISCV_VECTOR is "CORE")

--- include/elf/common.h
+++ include/elf/common.h
@@ -715,6 +715,8 @@
 					/*   note name must be "CORE".  */
 #define NT_RISCV_CSR    0x900		/* RISC-V Control and Status Registers */
 					/*   note name must be "GDB".  */
+#define NT_RISCV_VECTOR 0x900		/* RISC-V Vector Registers */
+					/*   note name must be "CORE".  */
 #define NT_SIGINFO	0x53494749	/* Fields of siginfo_t.  */
 #define NT_FILE		0x46494c45	/* Description of mapped files.  */
 
