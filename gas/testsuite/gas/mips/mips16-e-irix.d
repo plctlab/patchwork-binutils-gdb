0f8a90b2505d87f6b0de63937ab6a35dbfacda13
YunQiang Su <yunqiang.su@cipunited.com>
2023-08-15 19:13:51 +0800

    MIPS: Fix Irix gas testcases
    1. Add -mpdr option to cases, which expect .pdr section existing.
       Gas doesn't generate .pdr section for Irix objects by default.  See
       `int mips_flag_pdr` in tc-mips.c.
    
    2. Add irix-no-pdr.d testcase to make sure no .pdr section generates
       for Irix by default.
    
    3. Add and use call-nonpic-1-irix.d for Irix targets.
       Gas doesn't set O32 flags for Irix objects in e_flags.  See
       `use_e_mips_abi_o32` in gas/configure.ac.
    
    4. Add mips16-e-irix.d: g1 is marked as O on Irix.
       The Irix 5 and 6 assemblers set the type of any common symbol and
       any undefined non-function symbol to STT_OBJECT.
       See `mips_frob_symbol` in tc-mips.c.

--- gas/testsuite/gas/mips/mips16-e-irix.d
+++ gas/testsuite/gas/mips/mips16-e-irix.d
@@ -1,6 +1,11 @@
-#objdump: -rst -mips16
-#name: MIPS16 reloc 2
+#objdump: -rst --special-syms -mips16
+#name: MIPS16 reloc
 #as: -32 -mips16 -mips32 -mpdr
+#source: mips16-e.s
+
+# The Irix 5 and 6 assemblers set the type of any common symbol and
+# any undefined non-function symbol to STT_OBJECT.
+# See `mips_frob_symbol` in tc-mips.c.
 
 # Check MIPS16 reloc processing
 
@@ -16,11 +21,18 @@ SYMBOL TABLE:
 0+0000000 l    d  \.(mdebug|pdr)	0+0000000 (|\.mdebug|\.pdr)
 0+0000000 l    d  \.gnu\.attributes	0+0000000 (|\.gnu\.attributes)
 0+0000002 l       \.text	0+0000000 0xf0 l1
+0+0000004 l       \.text	0+0000000 0xf0 \.L1.*1
+0+0000000       O \*UND\*	0+0000000 g1
 
 
 RELOCATION RECORDS FOR \[foo\]:
 OFFSET +TYPE +VALUE
 0+0000000 R_MIPS_32         l1
+0+0000004 R_MIPS_32         l1
+0+0000008 R_MIPS_32         \.L1.*1
+0+000000c R_MIPS_32         \.L1.*1
+0+0000010 R_MIPS_32         g1
+0+0000014 R_MIPS_32         g1
 
 
 Contents of section \.text:
@@ -32,6 +44,7 @@ Contents of section \.MIPS\.abiflags:
  .*
  .*
 Contents of section foo:
- 0000 00000003 00000000 00000000 00000000  .*
+ 0000 00000000 00000008 00000000 00000003  .*
+ 0010 00000000 00000008 00000000 00000000  .*
 Contents of section \.gnu\.attributes:
  .*
